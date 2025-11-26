f4f648c274e2b96cde40cda42aa6ddcb3c32f9fe
Joao-Pedro-J <matheusitalo125@gmail.com>
2025-11-26 20:12:49 -0300

    feat: created entity `ProjectModel`

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/models/ProjectModel.java
@@ -0,0 +1,73 @@
+package dev.matheuslf.desafio.inscritos.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Date;
+
+@Entity
+public class ProjectModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O nome não pode ser nulo")
+    @Length(min = 3, max = 100, message = "O nome deve ter entre 3 a 100 caracteres")
+    private String name;
+
+    private String description;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    public ProjectModel() {
+    }
+
+    public ProjectModel(String name, String description, Date startDate, Date endDate) {
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
